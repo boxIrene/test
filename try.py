c23f8fe33f42604b7e01bbcbf25a593cb9572747
box_irene <48880377+boxIrene@users.noreply.github.com>
2019-04-29 09:54:06 +0800

    Create try.py
    Just for testing

---
+++ try.py
@@ -0,0 +1,10 @@
+class CNN:
+
+    def __init__(self, name):
+        self.name = name
+        print self.name
+    
+    def justTry(self):
+        print("My first git-hub file", " from", self.name)
+        
+N1 = CNN("Irene Chen")
